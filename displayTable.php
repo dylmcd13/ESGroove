d9e117fee847cffaa414635fa36883c04731b6f4
dylmcd13 <dylmcd13@gmail.com>
2021-11-04 21:05:57 -0400

    Added edits to displayTable.php and search.php that Cole gave me.

--- displayTable.php
+++ displayTable.php
@@ -1,14 +1,38 @@
 <?php
 
 if($connection=@mysqli_connect('localhost', 'cnussear1', 'cnussear1', 'ESGrooveDB')){
-	print '<p>Successfully connected to ESGroove DataBase.</p>';
-} 
+        print '<p>Successfully connected to ESGroove DataBase.</p>';
+}
 
-$artistName = $_POST[’artist’];
-$venueName = $_POST[’venue’];
-$query1="SELECT  * FROM bands  WHERE name ='$artistName'"; 
+$artistName = $_POST['artist'];
+$venueName = $_POST['venue'];
+
+
+$query1="SELECT  * FROM Bands  WHERE";
+
+ if($_POST['BandSearchFilter'] =="bandName") {
+
+         $query1 .= " name='$artistName'";
+
+        }
+ if($_POST['BandSearchFilter'] =="genre") {
+
+          $query1 .= " genre='$artistName'";
+
+        }
+
+ if($_POST['BandSearchFilter'] =="numMembers") {
+
+          $query1 .= " numMembers='$artistName'";
+
+        }
+ if($_POST['BandSearchFilter'] =="years") {
+
+          $query1 .= " yearsActive='$artistName'";
+
+        }
 $r=mysqli_query($connection, $query1);
-echo "<table border=’1’>
+echo "<table border='1'>
 <thead>
 <tr>
 <th> name </th>
@@ -19,20 +43,44 @@ echo "<table border=’1’>
 </thead>";
 
 while($row=mysqli_fetch_array($r)){
-	echo "<tr>";
-	echo "<td>" . $row[’name’] . "</td>";
-	echo "<td>" . $row[’genre’] . "</td>";
-	echo "<td>" . $row[’numMembers’] . "</td>";
-	echo "<td>" . $row['yearsActive'] . "</td>"; 
-	echo "</tr>";
+ echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['genre'] . "</td>";
+        echo "<td>" . $row['numMembers'] . "</td>";
+        echo "<td>" . $row['yearsActive'] . "</td>";
+        echo "</tr>";
 }
 
 echo "</table>";
-echo <br>;
-echo <br>;
-echo <br>;
+echo "<br>";
+echo "<br>";
 
-$query2 = "SELECT  * FROM Venues WHERE name='$venueName'";
+$query2 = "SELECT  * FROM Venues WHERE";
+ if($_POST['VenueSearchFilter'] =="venueName") {
+
+          $query2 .= " name ='$venueName'";
+
+        }
+ if($_POST['VenueSearchFilter'] =="established") {
+
+          $query2 .= " established ='$venueName'";
+
+        }
+ if($_POST['VenueSearchFilter'] =="established") {
+
+          $query2 .= " established ='$venueName'";
+
+        }
+ if($_POST['VenueSearchFilter'] =="type") {
+
+          $query2 .= " type ='$venueName'";
+
+        }
+ if($_POST['VenueSearchFilter'] =="address") {
+
+          $query2 .= " address ='$venueName'";
+
+        }
 $s=mysqli_query($connection, $query2);
 echo "<table border ='1'>
 <thead>
@@ -53,6 +101,7 @@ echo "<td>" . $row2['address'] . "</td>";
 }
 echo "</table>";
 
-mysqli_close($connection); 
+mysqli_close($connection);
 
 ?>
+
--- search.php
+++ search.php
@@ -1,11 +1,29 @@
 <html>
 <body>
 <form method=post action="displayTable.php">
-<label for="artist" >Enter Artist</label> <br>
-<input type="text" id = "artist" name="artist" placeholder="artist"><br>
-<label for="venue"  >Enter Venue</label> <br> 
-<input type="text" id = "venue"  name ="venue" placeholder="venue"><br> 
-<input type="submit">
+<label for="artist" >Artist Search</label> <br>
+<input type="text" id = "artist" name="artist">
+<label for="BandSearchFilter">Search By...</label>
+<select name="BandSearchFilter" id="BandSearchFilter" placeholder="Search by" >
+<option value="bandName">Band Name</option>
+<option value="genre">Genre</option>
+<option value="numMembers">Number of Members</option>
+<option value="years">Years Active</option>
+</select>
+<br>
+<label for="venue"  >Venue Search</label> <br>
+<input type="text" id = "venue"  name ="venue">
+<label for="VenueSearchFilter">Search By...</label>
+<select name="VenueSearchFilter" id="VenueSearchFilter" placeholder="Search by" >
+<option value="venueName">Venue Name</option>
+<option value="established">Established Year</option>
+<option value="type">Type of Venue</option>
+<option value="address">Address of Venue</option>
+</select>
+<br>
+
+	<input type="submit">
 </form>
 </body>
 </html>
+
