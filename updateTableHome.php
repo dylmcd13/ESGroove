608daef115311ab9bcbb60a85f1f94ce554311da
dylmcd13 <dylmcd13@gmail.com>
2021-11-19 09:50:12 -0500

    added update php files to be able to use with drop down box when on updateTableHome.php

--- updateTableHome.php
+++ updateTableHome.php
@@ -11,71 +11,39 @@
 
 
 <select name="table" id="table" onchange="javascript:location.href = this.value;">
-	<option value="default"selected>--Select--</option>
+	<option value=""selected>--Select--</option>
   	<option value="updateBands.php">Bands</option>
-  	<option value="Venues">Venues</option>
-  	<option value="Albums">Albums</option>
-  	<option value="Songs">Songs</option>
-  	<option value="Members">Members</option>
-  	<option value="Merch">Merch</option>
-  	<option value="Travelto">Travelto</option>
-  	<option value="Facebook">Facebook</option>
- 	<option value="Instagram">Instagram</option>
-  	<option value="Twitter">Twitter</option>
+  	<option value="updateVenue.php">Venues</option>
+  	<option value="updateAlbums.php">Albums</option>
+  	<option value="updateSongs.php">Songs</option>
+  	<option value="updateMembers.php">Members</option>
+  	<option value="updateMerch.php">Merch</option>
+  	<option value="updateTravelto.php">Travelto</option>
+  	<option value="updateFacebook.php">Facebook</option>
+ 	<option value="updateInstagram.php">Instagram</option>
+  	<option value="updateTwitter.php">Twitter</option>
 	</select> 
 
 
-<input type="button" value="Submit" onclick="myFunction()">
+<input type="button" value="Submit" onclick="form.submit()">
 
 
 <p id="test"></p>
 
 <script>
-function myFunction() {
-  var val = document.getElementById("table").value;
-  document.getElementById("test").innerHTML = val;
-  if(val == "Bands")
-  {
-	
-  }
-  else if(val == "Venues")
-  {
-  	document.getElementById("test").innerHTML = "Venues";
-  }
-  else if(val == "Albums")
-  {
-  	document.getElementById("test").innerHTML = "Albums";
-  }
-  else if(val == "Songs")
-  {
-  	document.getElementById("test").innerHTML = "Songs";
-  }
-  else if(val == "Members")
-  {
-  	document.getElementById("test").innerHTML = "Members";
-  }
-  else if(val == "Merch")
-  {
-  	document.getElementById("test").innerHTML = "Merch";
-  }
-  else if(val == "Travelto")
-  {
-        document.getElementById("test").innerHTML = "Travelto";
-  }
-  else if(val == "Facebook")
-  {
-        document.getElementById("test").innerHTML = "Facebook";
-  }
-  else if(val == "Instagram")
-  {
-        document.getElementById("test").innerHTML = "Instagram";
-  }
-  else if(val == "Twitter")
-  {
-        document.getElementById("test").innerHTML = "Twitter";
-  }
+
+function submit()
+{
+	if(document.getElementById("table").value == "Bands")
+	{
+		document.getElementById("table").value = "updateBands.php";
+	}
 }
+
 </script>
 
 
+
 </html>
+
+
