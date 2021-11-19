e8e3fbc3872c5cb6c13d8f02017cd8215b8021ad
Braiden Ryan <braidenjester@gmail.com>
2021-11-19 12:40:45 -0500

    added framework for delete

---
+++ deleteFromTable.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+
+<html>
+
+<h style="font-size: 20pt"><b> Delete Form Table </b></h>
+
+
+
+<?php
+
+/* ***UNCOMMENT WHEN PUTTING INTO ACADWEB1
+
+if($connection=@mysqli_connect('localhost', 'dmcdonald2', 'dmcdonald2', 'ESGrooveDB'))
+
+{
+
+	print 'SUCCESS';
+
+}
+
+else
+
+{
+
+	print 'UNSUCCESSFUL';
+
+}
+
+*/
+
+
+
+?>
+
+<br>
+
+<br>
+
+
+
+<label for="Table">Choose a Table:</label>
+
+
+
+<select name="table" id="table">
+
+	<option value="">--Select--</option>
+
+  	<option value="Bands">Bands</option>
+
+  	<option value="Venues">Venues</option>
+
+  	<option value="Albums">Albums</option>
+
+  	<option value="Songs">Songs</option>
+
+  	<option value="Members">Members</option>
+
+  	<option value="Merch">Merch</option>
+
+  	<option value="Travelto">Travelto</option>
+
+  	<option value="Facebook">Facebook</option>
+
+ 	<option value="Instagram">Instagram</option>
+
+  	<option value="Twitter">Twitter</option>
+
+</select> 
+
+
+
+
+
+
+
+</html>
