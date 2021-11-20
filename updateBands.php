72596bfa17f169b628302f8e60d7e7694c0de94e
dylmcd13 <dylmcd13@gmail.com>
2021-11-20 12:29:59 -0500

    made it so the drop down box adds the name to the text box

--- updateBands.php
+++ updateBands.php
@@ -18,7 +18,7 @@ session_start();
 
 
 
-<select name="table" id="table" onchange="javascript:location.href = this.value;" style="width: 800px;">
+<select name="table" id="table" onchange="insert()" style="width: 800px;">
         <option value=""selected>--Select--</option>
 
 <?php
@@ -26,21 +26,69 @@ session_start();
 $r=mysqli_query($connection, $showTableQuery);
 
 
-$mask = "|%-50.50s |%.5s |\n";
 
 while($row=mysqli_fetch_array($r)){
 	$name = $row['name'];
 	$genre = $row['genre'];
 	$numMembers = $row['numMembers'];
 	$yearsActive = $row['yearsActive'];
-	echo '<option value="' . $row['name'] . $row['genre'] . '"> ' . $name  . ' || ' . $genre . ' || ' . $numMembers . ' || ' . $yearsActive .  '</option>';
+	echo '<option value="' . $name.'"> ' . $name  . ' || ' . $genre . ' || ' . $numMembers . ' || ' . $yearsActive .  '</option>';
 
 }
 
 
 ?>
-
 </select>
+
+<br>
+<br>
+
+<form>
+<input type="text" id="name" name="name" size="50">
+<label for="name">Band Name</label>
+</form>
+<br>
+
+<form>
+<input type="text" id="genre" name="genre" size="50">
+<label for="genre">Genre</label>
+</form>
+<br>
+
+<form>
+<input type="text" id="numMembers" name="numMembers" size="3">
+<label for="numMembers">Number of Members</label>
+</form>
+<br>
+
+<form>
+<input type="text" id="yearsActive" name="yearsActive" size="3">
+<label for="yearsActive">Years Active</label>
+</form>
+<br>
+
+
+<script>
+
+function insert()
+{
+	$nameInsert = document.getElementById('table').value;
+	document.getElementById('name').value = $nameInsert;
+
+	$genreInsert
+}
+
+
+</script>
+
+
+
+
+
+
+
+
+
 
 
 
