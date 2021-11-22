18d7c6e7a4b23abc90bc7e66adf5347f58a1db11
Alex Hearne <ahearne1@gulls.salibury.edu>
2021-11-22 08:03:15 -0500

    For login  - session variable

---
+++ sessionLoggedIn.php
@@ -0,0 +1,29 @@
+
+<?php
+session_start();
+require 'connection.php'; //connection to ESGroove database
+global $connection; //global connection variable
+if (isset($_POST['email']) && isset($_POST['password'])){
+
+// Assigning posted values to variables.
+$username = $_POST['email'];
+$password = $_POST['password'];
+
+// Checking the values are existing in the database or not
+$query = "SELECT * FROM users WHERE username = '$username' && password = '$password'";
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$count = mysqli_num_rows($result);
+
+// If the posted values are equal to the database values, then session will be created for the user.
+if ($count > 0){
+$_SESSION['email'] = $username;
+}else{
+
+// If the login credentials do not match, an error message will be shown.
+echo "Invalid Login Credentials. Please try again.";
+}
+}
+if (isset($_SESSION['email'])) {
+$username = $_SESSION['email'];
+}
+?>
