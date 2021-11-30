838c301f5334328516e9ba2f70c5eba1cdab5e03
Alex Hearne <ahearne1@gulls.salibury.edu>
2021-11-29 22:36:30 -0500

    unset the variable upon logout as well.

--- logOut.php
+++ logOut.php
@@ -1,5 +1,6 @@
 <?php
 session_destroy(); //upon pressing logout button, we destroy the session and all of its data.
-header("location: home.php"); //Upon logout, redirect to home login page. This can be whatever our home login page is.
+unset($_SESSION['email']);
+header("location: session.html"); //Upon logout, redirect to home login page. This can be whatever our home login page is.
 //whatever home login page is, change to location: filename.xxx
 ?>
