d7a79f668fe31651d3853b2df82a5e840154e3e1
Ryanpoholsky <73671074+Ryanpoholsky@users.noreply.github.com>
2021-12-07 13:03:44 -0500

    Add files via upload

---
+++ insertAlbum.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+ <form action = 'insert.php' method = 'post'>
+Album Title:
+ <input type = "text" name="Album Title" id = albumTitle>
+Band Name:
+ <input type = "text" name = "Band Name" id =bandName>
+Number of Tracks:
+ <input type ="text" name = "Number of Tracks" id = numOfTracks>
+Album Length:
+ <input type ="text" name = "Album Length" id = albumLegnth>
+Release Year of Album:
+ <input type = "text" name = "Release Year" id = releaseYear>
+
+</form>
+</body>
+</html>
