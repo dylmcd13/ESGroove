2539769f081ca6657e812d6cf108977b7e3328e4
Cole Nussear <80608048+Nusse0712@users.noreply.github.com>
2021-12-08 13:36:56 -0500

    updated performances so  the search works in the tab
    Standby for more updates

--- searchPerformanceAdmin.php
+++ searchPerformanceAdmin.php
@@ -1,19 +1,20 @@
 <html>
 <head>
-        <link href="styles.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
 </head>
 <body>
     <a name="top"></a>
 <div>
-        <ul id="navbar">
-  <li id="navtab"><a   id="navlink" href="index.php">Home</a></li>
-  <li id="navtab"><a  id="navlink" href="Artists.php">Artists</a></li>
-  <li id="navtab"><a  id="navlink" href="Venues.php">Venues</a></li>
-  <li id="navtab"><a  id="navlink" href="Performances.php">Performances</a></li>
-  <li id="navtab"><a  id="navlink" href="Login.php">Login</a></li>
+    <ul id="navbar">
+  <li id="navtab"><a   id="navlink" href="indexAdmin.php">Home</a></li>
+  <li id="navtab"><a  id="navlink" href="ArtistsAdmin.php">Artists</a></li>
+  <li id="navtab"><a  id="navlink" href="VenuesAdmin.php">Venues</a></li>
+  <li id="navtab"><a  id="navlink" href="PerformancesAdmin.php">Performances</a></li>
+  <li id="navtab"><a  id="navlink" href="AdminMenu.php">Admin Menu</a></li>
+  <li id="navtab"><a  id="navlink" href="Logout.php">Logout</a></li>
   <li id="headerLogo">ES<span id="headerSpan">Groove</span></li>
 </ul>
- <hr style="height: 15px; background-color: #444; border-color: #444; margin: 0; padding: 0;">
+<hr style="height: 15px; background-color: #444; border-color: #444; margin: 0; padding: 0;">
 </div>
 <?php
 $connection=@mysqli_connect('localhost', 'dmcdonald2', 'dmcdonald2', 'ESGrooveDB'))
@@ -68,15 +69,17 @@ mysqli_close($connection);
                 <h3>ES<span>Groove</span></h3>
 
                 <p class="footer-links">
-                    <a id="footerLink" href="index.php">Home</a>
+                   <a id="footerLink" href="indexAdmin.php">Home</a>
                     
-                    <a id="footerLink" href="Artists.php">Artists</a>
+                    <a id="footerLink" href="ArtistsAdmin.php">Artists</a>
                     
-                    <a id="footerLink" href="Venues.php">Venues</a>
+                    <a id="footerLink" href="VenuesAdmin.php">Venues</a>
                     
-                    <a id="footerLink" href="Performances.php">Performances</a>
+                    <a id="footerLink" href="PerformancesAdmin.php">Performances</a>
                     
-                    <a id="footerLink" href="Login.php">Login</a>
+                    <a id="footerLink" href="AdminMenu.php">Admin Menu</a>
+
+                    <a id="footerLink" href="Logout.php">Logout</a>
                     
                     <a href="#top">Back to top of page</a>
                 </p>
