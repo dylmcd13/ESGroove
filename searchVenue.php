fdf73c5a3f87f5aed236bc24749249bff6a268bb
Cole Nussear <80608048+Nusse0712@users.noreply.github.com>
2021-12-08 12:27:08 -0500

    Add files via upload

--- searchVenue.php
+++ searchVenue.php
@@ -25,22 +25,22 @@ $venueName = $_POST['venue'];
 $query2 = "SELECT  * FROM Venues WHERE";
  if($_POST['VenueSearchFilter'] =="venueName") {
 
-          $query2 .= " name ='%$venueName%'";
+          $query2 .= " name =%'$venueName'%";
 
         }
  if($_POST['VenueSearchFilter'] =="established") {
 
-          $query2 .= " established ='$venueName'";
+          $query2 .= " established =%'$venueName'%";
 
         }
  if($_POST['VenueSearchFilter'] =="type") {
 
-          $query2 .= " type ='%$venueName%'";
+          $query2 .= " type = %'$venueName'%";
 
         }
  if($_POST['VenueSearchFilter'] =="address") {
 
-          $query2 .= " address ='%$venueName%'";
+          $query2 .= " address = %'$venueName'%";
 
         }
 $s=mysqli_query($connection, $query2);
@@ -66,7 +66,10 @@ echo "</table>";
 mysqli_close($connection);
 
 ?>
-
+<br>
+<br>
+<br>
+<br>
 </body>
 <footer class="footer-distributed">
 
