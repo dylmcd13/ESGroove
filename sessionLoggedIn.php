73c8a8c5e44cc275b5f29e83cac08f1379664fb3
Alex Hearne <ahearne1@gulls.salibury.edu>
2021-12-08 14:41:53 -0500

    Changed values from password to email

--- sessionLoggedIn.php
+++ sessionLoggedIn.php
@@ -3,11 +3,11 @@
 session_start();
 $connection=@mysqli_connect('localhost', 'dmcdonald2', 'dmcdonald2', 'ESGrooveDB'); //connection to ESGroove database
 global $connection; //global connection variable
-if (isset($_POST['userName']) && isset($_POST['passWord'])){
+if (isset($_POST['email']) && isset($_POST['password'])){
 
 // Assigning posted values to variables.
-$username = $_POST['userName'];
-$password = $_POST['passWord'];
+$username = $_POST['email'];
+$password = $_POST['password'];
 
 // Checking the values are existing in the database or not
 $query = "SELECT * FROM users WHERE username = '$username' && password = '$password'";
@@ -16,7 +16,7 @@ $count = mysqli_num_rows($result);
 
 // If the posted values are equal to the database values, then session will be created for the user.
 if ($count > 0){
-$_SESSION['userName'] = $username;
+$_SESSION['email'] = $username;
 header("location: AdminMenu.php"); //whatever the name of our home page is
 }else{
 header("location: Login.php");
@@ -26,11 +26,11 @@ echo "Invalid Login Credentials. Please try again.";
 } else {
 header("location: Login.php");
 }
-//if (isset($_SESSION['userName'])) {
-//$username = $_SESSION['userName'];
-//header("location: AdminMenu.php");
-//} else {
-//header("location: Login.php");
-//}
+if (isset($_SESSION['email'])) {
+$username = $_SESSION['email'];
+header("location: AdminMenu.php");
+} else {
+header("location: Login.php");
+}
 ?>
 
