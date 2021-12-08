b564aa89793cc7fb02b88936db85c43fb8bc5cfb
Cole Nussear <80608048+Nusse0712@users.noreply.github.com>
2021-12-08 12:17:11 -0500

    updated the search venues so it blends with the website

--- VenuesAdmin.php
+++ VenuesAdmin.php
@@ -22,7 +22,7 @@
 
 <fieldset id="searchSet">
  <legend id="searchLegend">Search Venues</legend>
- <form action="searchVenue.php">    
+ <form action="searchVenueAdmin.php">    
  <label for="VenueSearchFilter" id="searchLabel">Search By...</label>
  <select name="VenueSearchFilter" id="VenueSearchFilter" placeholder="Search by" >
  <option value="venueName">Venue Name</option>
